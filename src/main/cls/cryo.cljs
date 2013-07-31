a33f95261607953f40adf93fff75aeecd98b6615
qpiny <github@rejna.org>
2013-07-31 17:31:32 +0200

    daily commit

--- src/main/cls/cryo.cljs
+++ src/main/cls/cryo.cljs
@@ -6,20 +6,29 @@
             [goog.dom.ViewportSizeMonitor :as viewport-size]
             [goog.ui.Component]
             [goog.ui.SplitPane]
-            [goog.ui.SplitPane.Orientation]))
+            [goog.ui.SplitPane.Orientation :as orientation]))
+
+(defn- make-split
+  ([parent] (make-split parent orientation/HORIZONTAL))
+  ([parent orientation]
+    (let [splitpane (goog.ui.SplitPane.
+                    (goog.ui.Component.)
+                    (goog.ui.Component.)
+                    orientation)]
+        (.setHandleSize splitpane 2)
+        (.decorate splitpane parent)
+        splitpane)))
+        
 
 (defn ^:export build []
 
-  (let [lhs (goog.ui.Component.)
-        rhs (goog.ui.Component.)
-        splitpane1 (goog.ui.SplitPane. lhs rhs goog.ui.SplitPane.Orientation.HORIZONTAL)
-        update-ui (fn [size]
-                    (.setSize splitpane1 (new goog.math.Size (- size/width 100) (- size/height 100))))
+  (def splitter1 (make-split (dom/getElement "splitter1") orientation/HORIZONTAL))
+  (def splitter2 (make-split (dom/getElement "splitter2") orientation/VERTICAL))
+  (def splitter3 (make-split (dom/getElement "splitter3") orientation/HORIZONTAL))
+  
+  (let [update-ui #(.setSize splitter1 (new goog.math.Size (- (.-width %) 100) (- (.-height %) 100)))
         vsm (goog.dom.ViewportSizeMonitor.)]
-    (.setInitialSize splitpane1 100)
-    (.setHandleSize splitpane1 2)
-    (.decorate splitpane1 (dom/getElement "anotherSplitter"))
     (update-ui (dom/getViewportSize))
     (goog-events/listen vsm
                         event-type/RESIZE
-                        (fn [e] (update-ui (. vsm (getSize)))))))
+                        #(update-ui (.getSize vsm)))))
