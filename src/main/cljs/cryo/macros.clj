67b6ad4f359879551083d65c04c85acf2eef88fc
qpiny <github@rejna.org>
2013-08-26 21:29:54 +0200

    Daily commit

--- src/main/cljs/cryo/macros.clj
+++ src/main/cljs/cryo/macros.clj
@@ -1,33 +1,104 @@
 (ns cryo.macros
   (:require [dommy.macros]))
 
-;(declare dommy.template/->node-like)
+(defn rmap [f e]
+  (if (and (coll? e) (not (map? e)))
+    (f (into (empty e) (map #(rmap f %) e)))
+    (f e)))
 
-(declare anode)
+(defn get-dom [x] (if-let [d (:dom x)] d x))
+(defn get-comp [x] (if-let [d (:comp x)] d x))
 
-(defmacro widget [wtype a1 & args]
-  (condp = ~wtype
-;    :zip (let [[title# & content#] args
-;               n (widget [:node [:div title# content#]])]
-;           `(goog.ui.Zippy. title# content# true)
-;           n)
-    :split `(let [[orient# first-pane# second-pane#] ~args
-                  first-component# (.-component first-pane#)
-                  second-component# (.-component second-pane#)
-                  splitpane# (goog.ui.SplitPane. first-component# second-component#)]
-              (doto splitpane#
-                (.createDom)
-                (.setHandleSize 2))
-              (ClosureWidget. splitpane#))
-    ;:node (dommy.macros/node ~args)))
-    ;`(cryo.ui/NodeWidget. (dommy.macros/node ~args))))
-    :node (if (empty? args)
-            (str "empty args > " wtype ":" a1)
-            (str "non-empty args > " wtype ":" a1 " / " (widget (first args) (second args)) " -> " ;(widget ~@args)
-                 ))))
+(defn dom->comp [d]
+  (let [s (gensym "d2c")]
+    `(do (let [~s (goog.ui.Component.)]
+           (.createDom ~s)
+           (dc/append! (.getContentElement ~s) ~d)))))
 
-(defmacro aa [a & b]
-  `(str "aa" ~a "bb"))
+(defn comp->dom [c]
+  `(.getContentElement ~c))
+
+(defn build-widget [args]
+;  (let [debug 
+  (if-let [wtype (and (coll? args) (not (map? args)) (first args))]
+    (condp = wtype
+      :dom (let [param (into [] (map get-dom (rest args)))
+                 dom `(dommy.macros/node ~@param)]
+             {:dom dom
+              :comp (dom->comp dom)})
+      :split (let [[_ opt first-pane second-pane] args
+                   fp (get-comp first-pane)
+                   sp (get-comp second-pane)
+                   comp `(goog.ui.SplitPane. ~fp ~sp)]
+               {:dom (comp->dom comp)
+                :comp comp})
+      :zip (let [[_ opt header content] args
+                 h (get-dom header)
+                 c (get-dom content)
+                 dom `(do
+                        (dommy.macros/node [:div ~h ~c])
+                        (goog.ui.Zippy. ~h ~c))]
+             {:dom dom
+              :comp (dom->comp dom)})
+      args)
+    args)
+;    ]
+;    (println "debug" args "->" debug)
+;    debug)
+    )
+
+(defmacro widget [args]
+  (let [w (rmap build-widget args)]
+    ;`(js/alert (str ~@w))))
+    `(do ~@(:dom w))))
+                       
+
+;(defmacro widget [args]
+;  (rmap #(do
+;           ;(println (str % " => " (and (coll? %) (first %)) ":" (if (and (coll? %) (first %)) "YES" "NO")))
+;           (if-let [a (and (coll? %) (first %))]
+;           (do ;(binding [*err* *out*] (println (str % " -> " a)))
+;             (condp = a
+;               :split `(let [[_ opt# first-pane# second-pane#] ~args
+;                           orientation# (get opt# :orientation "vertical")
+;                           first-component# (.-component first-pane#)
+;                           second-component# (.-component second-pane#)
+;                           splitpane# (goog.ui.SplitPane. first-component# second-component#)]
+;                         (println "split!")
+;                         (doto splitpane#
+;                           (.createDom)
+;                           (.setHandleSize 2))
+;                         (ClosureWidget. splitpane#))
+;               :zip `(let [[_ opt# title# content#] ~args
+;                         n (node [:div title# content#])]
+;                     (goog.ui.Zippy. (.-elem title#) (.-elem content#))
+;                     n)
+;             :node `(let [[dummy# & param#] ~args]
+;                      (dommy.macros/node param#))
+;             `[:default %]) ;(println "invalid token :" a))
+;             )
+;           %)) args))
+;  `(str ~(rmap #(if (= % :node) "NODE" %) args)))
+
+;    :node (if 
+;    "node" `(let [debug1# '~args
+;                  debug2# '~wtype
+;                  debug3# '~a1]
+;              ~(if (empty? args)
+;                  `(str (str "js/alert" "plop"))
+;                  `(do
+;                     (.createElement js/document ~wtype)
+;                     (dommy.macros/node ~a1)
+;                    ;(str (str "js/alert" ">>" ~wtype "|" ~a1 "--"))
+;                    (widget ~@args)
+;                    )
+;                  ;`(str "empty args > " ~wtype ":" ~a1)
+;                  ;`(str "non-empty args > " ~wtype ":" ~a1 " / " (widget ~@args))
+;                 ))
+;    `(str "invalid type : " ~wtype)))
+;
+;(defmacro aa [a & b]
+;  `(str "aa" ~a "bb"))
 
 ;(defn anode [t a & o] 
 ;  (let [[nt na & no] o
--- src/main/cljs/cryo/ui.cljs
+++ src/main/cljs/cryo/ui.cljs
@@ -1,7 +1,7 @@
 (ns cryo.ui
   (:use-macros
      [dommy.macros :only [node sel sel1]]
-     [cryo.macros :only [aa widget]])
+     [cryo.macros :only [aa widget w]])
   (:require [logger]
             [dommy.template]
            ; [goog.ui.SplitPane.Orientation :as orientation]
@@ -11,40 +11,50 @@
             [goog.ui.SplitPane]
             [goog.events :as ev]))
 
-(defprotocol IClosureWidget
-  (-component [this]))
-
-(defrecord NodeWidget [data]
-  dommy.template/PElement
-  (-elem [this] data)
-  IClosureWidget
-  (-component [this]
-    (let [comp (goog.ui.Component.)]
-      (.createDom comp)
-      (dc/append! (.getContentElement comp) data)
-      comp)))
-
-(defrecord ClosureWidget [data]
-  dommy.template/PElement
-  (-elem [this] (.getElement data))
-  IClosureWidget
-  (-component [this] data))
+;(defprotocol IClosureWidget
+;  (-component [this]))
+;
+;(defrecord NodeWidget [data]
+;  dommy.template/PElement
+;  (-elem [this] data)
+;  IClosureWidget
+;  (-component [this]
+;    (let [comp (goog.ui.Component.)]
+;      (.createDom comp)
+;      (dc/append! (.getContentElement comp) data)
+;      comp)))
+;
+;(defrecord ClosureWidget [data]
+;  dommy.template/PElement
+;  (-elem [this] (.getElement data))
+;  IClosureWidget
+;  (-component [this] data))
 
 (defn mainXX []
-  (widget :node "kk" :node "ll")
+  ;(widget "node" "kk" "nodde" "lel")
   ;(node [:div "plop"])
   ;(aa (str ":n" "ode1") :node2)
-  )
-;  (widget :split "vertical"
-;           (widget :split "horizontal"
-;                    (widget :zip
-;                             (widget :node [:h2.zip-header "Inventory"])
-;                             (widget :node [:div
-;                                             [:div "Date "   [:span#inv-date "unknown"]]
-;                                             [:div "Status " [:span#inv-status "unknown"] [:div#inv-refresh]]
-;                                             [:div#snapshot-list]]))
-;                    (widget :node [:div "coucou2"]))
-;           (widget :node [:div "logs"])))
+  (def root (widget [:split {:orientation "vertical"}
+                     [:split {:orientation "horizontal"}
+                      [:zip []
+                       [:dom [:h2.zip-header "Inventory"]]
+                       [:dom [:div
+                              [:div "Date "   [:span#inv-date "unknown"]]
+                              [:div "Status " [:span#inv-status "unknown"] [:div#inv-refresh]]
+                              [:div#snapshot-list]]]]
+                      [:dom
+                       [:div
+                        [:table [:tbody
+                                 [:tr [:th "ID"] [:td#snap-id]]
+                                 [:tr [:th "Date"] [:td#snap-date]]
+                                 [:tr [:th "Size"] [:td#snap-size]]
+                                 [:tr [:th "Status"] [:td#snap-status]]]]
+                        [:div#action-panel
+                         [:div#snap-delete]
+                         [:div#snap-download]
+                         [:div#snap-upload]
+                         [:div#snap-duplicate]]]]]
+                     [:dom [:div.console "logs ..."]]])))
 
 ;(defn snapshot-select [element event snap]
 ;  (doseq [s (sel (sel1 :#snapshot-list) :.selected)]
@@ -53,13 +63,14 @@
 ;
 ;(defn snapshot [snap]
 ;  (let [element (node
-;                  [:div.snaphost
+;  [:div.snaphost
 ;                   [:div.snap-id ("id" snap)]
 ;                   [:div.snap-description ("description" snap)]
 ;                   [:div.snap-date ("creationDate" snap)]
 ;                   [:div.snap-status ("status" snap)]
 ;                   [:div.snap-size ("size" snap)]
 ;                   [:div.snap-checksum ("checksum" snap)]])]
+;                  
 ;    (logger/log "Add event listener on click")
 ;    (logger/log snap)
 ;    (logger/log element)
@@ -67,14 +78,4 @@
 ;    element))
 ;
 ;(defn main-panel-header []
-;  (node [:div
-;         [:table [:tbody
-;                  [:tr [:th "ID"] [:td#snap-id]]
-;                  [:tr [:th "Date"] [:td#snap-date]]
-;                  [:tr [:th "Size"] [:td#snap-size]]
-;                  [:tr [:th "Status"] [:td#snap-status]]]]
-;         [:div#action-panel
-;          [:div#snap-delete]
-;          [:div#snap-download]
-;          [:div#snap-upload]
-;          [:div#snap-duplicate]]]))
+;  (node 
